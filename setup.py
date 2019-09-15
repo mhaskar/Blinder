36c18ac94ab2f3e7e4148aa62e7efb2e643b5b14
askar <m.askar@isecur1ty.org>
2019-09-15 10:14:03 +0300

    fixed license

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: GPL V3.0',
+    'License :: License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 2.7'
   ],
 )
