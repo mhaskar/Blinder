ceb47a2dc3331f935ebf3bb43365ec301456b410
askar <m.askar@isecur1ty.org>
2019-09-15 10:05:35 +0300

    fixed line issue

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
   packages = ['Blinder'],
   version = 'v0.1',
   license='GPL V3.0',
-  description = 'Blidner is a small python library to automate time-based blind SQL injection by using a pre defined queries as a functions to automate a rapid PoC development.
-',   # Give a short description about your library
+  description = 'Blidner is a small python library to automate time-based blind SQL injection by using a pre defined queries as a functions to automate a rapid PoC development.',
   author = 'Askar',
   author_email = 'm.askar@isecur1ty.org',
   url = 'https://github.com/user/reponame',
